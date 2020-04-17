592806e9a5f5d4c34fee67a77149853eaffe9525
yoavm448 <yoavm448@gmail.com>
2020-04-17 20:34:07 +0300

    Generalize
    Use post-command-hooks instead of transient maps

--- auto-latex-snippets.el
+++ auto-latex-snippets.el
@@ -19,19 +19,27 @@
 ;;
 ;;; Code:
 
-(defun als-set-expanding-ligature (keymap key expansion)
-  "Set KEY (a 2-char string) to expand into EXPANTION in KEYMAP."
-  (define-key keymap (substring key 0 1)
-    (lambda () (interactive)
-      (insert (this-command-keys))
-      (set-transient-map
-       (let ((keymap (make-sparse-keymap)))
-         (define-key keymap (substring key 1 2)
-           (lambda () (interactive)
-             (when (eq (char-before) (aref key 0)) ; sanity check
-               (delete-char -1)
-               (insert expansion))))
-         keymap)))))
+
+(defun als-make-prefix-map (keymap key expansion)
+  "Bind KEY as extended prefix in KEYMAP to EXPANTION.
+
+EXPANTION must either be a string or function."
+  (let ((incomplete-key (substring key 0 -1))
+        (orig-keymap keymap))
+    (mapc (lambda (c)
+            (setq keymap (or (lookup-key keymap (string c))
+                             (define-key keymap (string c) (make-sparse-keymap)))))
+          incomplete-key)
+    (define-key keymap (substring key -1)
+      (cond ((stringp expansion)
+             (lambda () (interactive)
+               (delete-char (- (length key)))
+               (insert expansion)))
+            ((functionp expansion)
+             expansion)
+            (t
+             (error "Expantion must be either a string or function"))))
+    orig-keymap))
 
 (defun als-set-expanding-ligatures (keymap key-expantions)
   "Set multiple expantions for `als-set-expanding-ligature'.
@@ -39,39 +47,99 @@
 KEYMAP is passed to `als-set-expanding-ligature', and
 KEY-EXPANTIONS should be an alist of (key . expantion)."
   (dolist (key-expansion key-expantions)
-    (als-set-expanding-ligature keymap
-                                (car key-expansion)
-                                (cdr key-expansion))))
+    (als-make-prefix-map keymap
+                         (car key-expansion)
+                         (cdr key-expansion))))
 
 
 
-(defvar auto-latex-snippets-mode-map
+(defvar als-prefix-map
   (let ((keymap (make-sparse-keymap)))
     (als-set-expanding-ligatures
      keymap
-     '(("=>" . "\\implies")
-       ("==" . "&=")
-       ("~=" . "\\approx")
-       ("~~" . "\\sim")
-       ("~~" . "\\sim")
-       (">=" . "\\geq")
-       ("<=" . "\\leq")
-       (">>" . "\\gg")
-       ("<<" . "\\ll")
-       ("xx" . "\\times")
-       ("**" . "\\cdot")
-       ;; ("to" . "\\to")
-       ("->" . "\\to")
-       ("+-" . "\\pm")
-       ("-+" . "\\mp")))
+     '(
+       ("a1"       . "a_1")
+       ("a_11"     . "a_{11}")
+       ("a"        . "^ a^")
+       ("a^11"     . "a^{11}")
+       ("a+"       . "a +")
+       ("a"        . "+b a + b")
+       ("..."      . "\\dots")
+       ("=>"       . "`\\implies")
+       ("=<"       . "\\impliedby")
+       ;; ("//"       . "\\frac{}{}")
+       ;; (".../"     . "\\frac{...}{}")
+       ("iff"      . "\\iff")
+       ("inn"      . "\\in")
+       ("notin"    . "\\not\\in")
+       ("!="       . "\\neq")
+       ("=="       . "&=")
+       ("~="       . "\\approx")
+       ("~~"       . "\\sim")
+       (">="       . "\\geq")
+       ("<="       . "\\leq")
+       (">>"       . "\\gg")
+       ("<<"       . "\\ll")
+       ("xx"       . "\\times")
+       ("**"       . "\\cdot")
+       ("to"       . "\\to")
+       ("->"       . "\\to")
+       ("|->"      . "\\mapsto")
+       ("!>"       . "\\mapsto")
+       ("sin"      . "\\sin")
+       ;; ("v,."      . "\\vec{v}")
+       ;; ("abar"     . "\\overline{a}")
+       ;; ("ahat"     . "\\hat{a}")
+       ;; ("a~"       . "\\tilde{a}")
+       ;; ("a."       . "\\dot{a}")
+       ;; ("a.."      . "\\ddot{a}")
+       ;; ("...\\)a"  . "...\\) a")
+       ("\\\\\\" "\\setminus")
+       ("pmat"     . "pmatrix")
+       ("part"     . "\\frac{\\partial }{\\partial }")
+       ;; ("sq"       . "\\sqrt{}")
+       ("sr"       . "^2")
+       ("cb"       . "^3")
+       ("EE"       . "\\exists")
+       ("AA"       . "\\forall")
+       ("aii"      . "a_i")
+       ;; ("aip1"     . "a_{i+1}")
+       ;; ("set"      . "\\{ \\}")
+       ("||"       . "\\mid")
+       ("<>"       . "\\diamond")
+       ("case"     . "cases env.")
+       ;; ("st"       . "\\text{s.t.}")
+       ("+-"       . "\\pm")
+       ("-+"       . "\\mp")
+       ;; ("nCr"      . "\\binom{n}{r}")
+       ))
     keymap)
   "TODO.")
+
+(defvar als-current-prefix-map als-prefix-map
+  "TODO.")
+
+(defun als-post-self-insert-hook ()
+  "TODO."
+  (let ((k (lookup-key als-current-prefix-map (this-command-keys))))
+    (setq als-current-prefix-map
+          (cond ((null k)
+                 als-prefix-map)
+                ((functionp k)
+                 (funcall k)
+                 als-prefix-map)
+                (t ;; (keymapp k)
+                 k)))))
+
+
 
 ;;;###autoload
 (define-minor-mode auto-latex-snippets-mode
   "TODO."
   :init-value nil
-  :keymap auto-latex-snippets-mode-map)
+  (if auto-latex-snippets-mode
+      (add-hook 'post-self-insert-hook #'als-post-self-insert-hook)
+    (remove-hook 'post-self-insert-hook #'als-post-self-insert-hook)))
 
 (provide 'auto-latex-snippets)
 ;;; auto-latex-snippets.el ends here
